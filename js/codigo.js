0a87090843c8f7e8f7a721d07c30277b00325a47
Ibai Trueba <axiailus@gmail.com>
2019-04-30 14:06:39 +0200

    otros ejemplos y correcciones

--- js/codigo.js
+++ js/codigo.js
@@ -6,6 +6,15 @@ document.getElementById("copyanio").innerHTML = anio;
 let y = new Date();
 document.getElementById("footer").innerHTML = '&COPY;' + y.getFullYear() + ' Página realizada por: Ibai Trueba Campo.'; //nos devuelve el año en el que estamos en el mensaje del footer.
 
+/* function fechaHora() {
+    let fecha = new Date();
+    let dia, hora;
+    let semana = ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"];
+    dia = semana[fecha.getDay()];
+    hora = fecha.getHours()  + ':' + (fecha.getMinutes()<10?'0':'') + fecha.getMinutes():
+    document.getElementById("fecha").innerHTML = '<strong>RESULTADOS:</strong> Hoy es: ' + dia + '. La hora actual es ' + hora + '.';
+*/
+
 let dia = '';
 switch (new Date().getDay()) {
   case 0:
@@ -31,22 +40,23 @@ switch (new Date().getDay()) {
     break;
 }
 function date() {
-  document.getElementById("fecha").innerHTML = 'Hoy es ' + dia + '. La hora actual es ' + y.getHours(); + ':' + y.getMinutes();
+  document.getElementById("fecha").innerHTML = '<strong>RESULTADOS:</strong> Hoy es ' + dia + '. La hora actual es ' + y.getHours() + ':' + y.getMinutes();
 } //Esta es la función a la que llama el botón de Fecha y Hora.
+
 function turn() {
-  let n = '0123456789';
-  let nArr = n.split('');
-  nArr.reverse();
-  nArr.join('');
-  n = prompt("Please enter one number");
+ let numero = prompt("Introduzca un número de más de un dígito");
+ let invertido;
+ if  (numero != null && numero != undefined && numero != '' && numero.length > 1) {
+   invertido =  invertir(numero);
+   alert("El número " + numero + "invertido es " + invertido + '.');
+ } else {
+   alert("Lo que ha introducido no es válido. Vuelva a intentarlo.");
+ }
 }
 
-/*function turn() {
-  let number = prompt("Please enter one number", "32243");
-  if (number != null) {
-   
-  }
+/*funcion invertir(porCadena) {
+  let arr = porCadena.split('');
+  arr.reverse();
+  return arr.join('');
 }*/
-
-
 
